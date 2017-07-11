08966a5baf12ea5d5feb1dbbfa3176fae688a67b
dominikduda <gospodorz@gmail.com>
2017-07-11 10:51:29 +0200

    Remove unnecessary expects, add empty lines

--- spec/rspec/matchers/be_record_matcher_spec.rb
+++ spec/rspec/matchers/be_record_matcher_spec.rb
@@ -8,22 +8,21 @@ describe "be_record matcher" do
   context "with example to pass" do
     it "ensures that record is of correct kind" do
       user = User.create(name: "John")
-      expect do
-        expect(user).to be_record(User.last)
-      end.not_to fail
+
+      expect(user).to be_record(User.last)
     end
 
     it "ensures that record primary key match" do
       user = User.create(name: "Alex")
-      expect do
-        expect(user).to be_record(user)
-      end.not_to fail
+
+      expect(user).to be_record(user)
     end
   end
 
   context "with example to fail" do
     it "ensures that record is of correct kind" do
       user = User.create(name: "John")
+
       expect do
         expect(user).to be_record(DummyClass.last)
       end.to fail_with("Expected the record to be kind of DummyClass but was User instead.")
@@ -32,6 +31,7 @@ describe "be_record matcher" do
     it "ensures that record primary key match" do
       user = User.create(name: "Alex")
       last_user = User.create(name: "Michael")
+
       expect do
         expect(user).to be_record(last_user)
       end.to fail_with("Expected id of the record to eql #{last_user.id} but was #{user.id}")
