d393efd2e87b01f58c5f682c321c213aeaf0e1d7
dominikduda <gospodorz@gmail.com>
2017-07-11 10:44:03 +0200

    Implement be_record_matcher, add specs for it

--- lib/rspec/matchers/be_record_matcher.rb
+++ lib/rspec/matchers/be_record_matcher.rb
@@ -1,6 +1,25 @@
 RSpec::Matchers.define :be_record do |record|
   match do |actual|
-    expect(actual).to be_kind_of(record.class)
-    expect(actual).to have_attributes(record.class.primary_key => record[record.class.primary_key])
+    @failure_message = nil
+
+    begin
+      expect(actual).to be_kind_of(record.class)
+    rescue RSpec::Expectations::ExpectationNotMetError
+      @failure_message = "Expected the record to be kind of #{record.class} but was #{actual.class} instead."
+      return false
+    end
+
+    begin
+      expect(actual).to have_attributes(record.class.primary_key => record[record.class.primary_key])
+    rescue RSpec::Expectations::ExpectationNotMetError
+      @failure_message = "Expected id of the record to eql #{record.id} but was #{actual.id}"
+      return false
+    end
+
+    true
+  end
+
+  failure_message do
+    @failure_message
   end
 end
