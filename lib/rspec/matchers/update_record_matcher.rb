8d4fe4702fa00e1d851da5a82a13b1da43135f93
Dawid Pośliński <dposlinski@gmail.com>
2017-07-11 10:42:35 +0200

    Update record matcher base

--- lib/rspec/matchers/update_record_matcher.rb
+++ lib/rspec/matchers/update_record_matcher.rb
@@ -8,12 +8,19 @@ RSpec::Matchers.define :update_record do |resource, attributes|
     end
 
     first_attribute_key = attributes.keys.first
+
     initial_matcher = change { resource.reload.public_send(first_attribute_key) }.to(attributes[first_attribute_key])
+
     update_all_attributes = attributes.except(first_attribute_key).inject(initial_matcher) do |current_matcher, (attribute_name, expected_value)|
-      current_matcher.and change { resource.public_send(attribute_name) }.to(expected_value)
+        current_matcher.and change { resource.public_send(attribute_name) }.to(expected_value)
     end
 
-    expect { actual.call }.to update_all_attributes
+    begin
+      expect { actual.call }.to update_all_attributes
+    rescue RSpec::Expectations::ExpectationNotMetError => e
+      @failure_message = "Record not updated."
+      return false
+    end
 
     if using_form?
       expect(form_klass).to have_received(:new).with(have_attributes(id: resource.id), kind_of(Hash))
@@ -25,6 +32,10 @@ RSpec::Matchers.define :update_record do |resource, attributes|
   def using_form(form_klass)
     @form_klass = form_klass
     self
+  end
+
+  failure_message do
+    @failure_message
   end
 
   def using_form?
